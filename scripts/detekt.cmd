dd4268f72d015b4adb71c72a1d530f2e204a341d
Michael Akinde <michael.akinde@statnett.no>
2019-11-15 00:30:37 +0100

    Fix symbolic links in phabricator script

--- scripts/detekt.cmd
+++ scripts/detekt.cmd
@@ -1,8 +1,8 @@
 @echo off
 :: Need to do an if-else in order to handle the idiotic handling of paths in Windows
 :: i.e., %~dp0 sometimes returns the bin directory and sometimes the working directory.
-if exist %~dp0/../detekt/detekt-cli-1.0.0-RC12-all.jar (
-    java -jar %~dp0/../detekt/detekt-cli-1.0.0-RC12-all.jar %*
+if exist %~dp0/../detekt/detekt.jar (
+    java -jar %~dp0/../detekt/detekt.jar %*
 ) else (
-    java -jar %~dp0/../phabricator/elhubdev/detekt/detekt-cli-1.0.0-RC12-all.jar %*
+    java -jar c:/ProgramData/Phabricator/detekt/detekt.jar %*
 )
