2096cde93d5caa4d960e94bb85cd7451c784ed34
Michael Akinde <michael.akinde@statnett.no>
2019-11-18 15:54:02 +0100

    Correct path of config file

--- scripts/checkstyle.cmd
+++ scripts/checkstyle.cmd
@@ -2,7 +2,7 @@
 :: Need to do an if-else in order to handle the idiotic handling of paths in Windows
 :: i.e., %~dp0 sometimes returns the bin directory and sometimes the working directory.
 if exist %~dp0/../lib/checkstyle.jar (
-    java -jar %~dp0/../lib/checkstyle.jar -c=../checkstyle-config.xml %*
+    java -jar %~dp0/../lib/checkstyle.jar -c=../lint/checkstyle-config.xml %*
 ) else (
-    java -jar c:/ProgramData/Phabricator/libchecks/checkstyle.jar -c=../checkstyle-config.xml %*
+    java -jar c:/ProgramData/Phabricator/libchecks/checkstyle.jar -c=../lint/checkstyle-config.xml %*
 )
