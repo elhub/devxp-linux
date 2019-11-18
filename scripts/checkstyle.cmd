c71f84f751acc797442cf236eafe25e0383feba4
Michael Akinde <michael.akinde@statnett.no>
2019-11-18 13:20:27 +0100

    Reorganize files to ensure arcanist can be pulled

--- scripts/checkstyle.cmd
+++ scripts/checkstyle.cmd
@@ -1,8 +1,8 @@
 @echo off
 :: Need to do an if-else in order to handle the idiotic handling of paths in Windows
 :: i.e., %~dp0 sometimes returns the bin directory and sometimes the working directory.
-if exist %~dp0/../checkstyle/checkstyle.jar (
-    java -jar %~dp0/../checkstyle/checkstyle.jar %*
+if exist %~dp0/../lib/checkstyle.jar (
+    java -jar %~dp0/../lib/checkstyle.jar %*
 ) else (
-    java -jar c:/ProgramData/Phabricator/checkstyle/checkstyle.jar %*
+    java -jar c:/ProgramData/Phabricator/lib/checkstyle.jar %*
 )
--- scripts/detekt.cmd
+++ scripts/detekt.cmd
@@ -2,7 +2,7 @@
 :: Need to do an if-else in order to handle the idiotic handling of paths in Windows
 :: i.e., %~dp0 sometimes returns the bin directory and sometimes the working directory.
 if exist %~dp0/../detekt/detekt.jar (
-    java -jar %~dp0/../detekt/detekt.jar %*
+    java -jar %~dp0/../lib/detekt.jar %*
 ) else (
-    java -jar c:/ProgramData/Phabricator/detekt/detekt.jar %*
+    java -jar c:/ProgramData/Phabricator/lib/detekt.jar %*
 )
