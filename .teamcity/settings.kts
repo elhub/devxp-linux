96a99c2b629a8ac82445bc7304d29144c68897e9
Michael Akinde <michael.akinde@statnett.no>
2021-06-23 17:18:29 +0200

    Updated TeamCity imports

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,21 +1,15 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildFeatures
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
-import jetbrains.buildServer.configs.kotlin.v2019_2.Trigger
-import jetbrains.buildServer.configs.kotlin.v2019_2.VcsRoot
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.SshAgent
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 import no.elhub.common.build.configuration.AutoRelease
 import no.elhub.common.build.configuration.CodeReview
-import no.elhub.common.build.configuration.SonarScan
-import no.elhub.common.build.configuration.constants.GlobalTokens
 import no.elhub.common.build.configuration.ProjectType
-import no.elhub.common.build.configuration.ProjectType.ANSIBLE
+import no.elhub.common.build.configuration.PublishDocs
+import no.elhub.common.build.configuration.SonarScan
+import no.elhub.common.build.configuration.UnitTest
 
 version = "2020.2"
 
