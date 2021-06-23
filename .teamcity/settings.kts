71519dba874cfbad142a332f8a0e60b70149b98c
Michael Akinde <michael.akinde@statnett.no>
2021-06-23 17:20:00 +0200

    Updated TeamCity imports

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.SshAgent
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
@@ -7,9 +8,9 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.version
 import no.elhub.common.build.configuration.AutoRelease
 import no.elhub.common.build.configuration.CodeReview
 import no.elhub.common.build.configuration.ProjectType
-import no.elhub.common.build.configuration.PublishDocs
 import no.elhub.common.build.configuration.SonarScan
-import no.elhub.common.build.configuration.UnitTest
+import no.elhub.common.build.configuration.constants.GlobalTokens
+
 
 version = "2020.2"
 
