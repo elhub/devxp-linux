c66c7890b238d2f2ae6422ce6b2ae6bf22ae5b6a
Sergii Prodan <serpro@disroot.org>
2022-06-27 10:56:33 +0200

    Remove AutoRelease from TC settings
    Summary: We're not really releasing anything here.
    
    Test Plan: `cd .teamcity && mvn clean compile`
    
    Reviewers: O5 elhub/devxp, michael.akinde
    
    Reviewed By: O5 elhub/devxp, michael.akinde
    
    Subscribers: teamcity
    
    Differential Revision: https://phabricator.elhub.cloud/D6725

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,16 +1,12 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.SshAgent
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
-import no.elhub.common.build.configuration.AutoRelease
 import no.elhub.common.build.configuration.CodeReview
 import no.elhub.common.build.configuration.ProjectType
 import no.elhub.common.build.configuration.PublishDocs
 import no.elhub.common.build.configuration.SonarScan
-import no.elhub.common.build.configuration.constants.GlobalTokens
 
 version = "2022.04"
 
@@ -18,7 +14,6 @@ project {
 
     val projectId = "no.elhub.devxp:devxp-linux"
     val projectType = ProjectType.ANSIBLE
-    val artifactoryRepository = "elhub-bin-release-local"
 
     params {
         param("teamcity.ui.settings.readOnly", "true")
@@ -30,61 +25,30 @@ project {
         sonarId = projectId
     )
 
-    val buildChain = sequential {
-
-        buildType(
-            SonarScan(sonarScanConfig)
-        )
-
-        val githubAuth = SshAgent({
-            teamcitySshKey = "teamcity_github_rsa"
-            param("secure:passphrase", GlobalTokens.githubSshPassphrase)
-        })
-
-        buildType(
-            AutoRelease(
-                AutoRelease.Config(
-                    vcsRoot = DslContext.settingsRoot,
-                    type = projectType,
-                    sshAgent = githubAuth
-                )
-            ) {
-                triggers {
-                    vcs {
-                        branchFilter = "+:<default>"
-                        quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
-                    }
-                }
-            })
-
-        buildType(
-            PublishDocs(
-                PublishDocs.Config(
-                    vcsRoot = DslContext.settingsRoot,
-                    type = projectType,
-                    dest = "devxp/devxp-linux"
-                )
-            ) {
-                triggers {
-                    vcs {
-                        branchFilter = "+:<default>"
-                        quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
-                    }
-                }
-            })
-
-    }
-
-    buildChain.buildTypes().forEach { buildType(it) }
-
-    buildType(
-        CodeReview(
-            CodeReview.Config(
-                vcsRoot = DslContext.settingsRoot,
-                type = projectType,
-                sonarScanConfig = sonarScanConfig
-            )
+    val codeReview = CodeReview(
+        CodeReview.Config(
+            vcsRoot = DslContext.settingsRoot,
+            type = projectType,
+            sonarScanConfig = sonarScanConfig
         )
     )
 
+    val sonarScan = SonarScan(sonarScanConfig)
+
+    val publishDocs = PublishDocs(
+        PublishDocs.Config(
+            vcsRoot = DslContext.settingsRoot,
+            type = projectType,
+            dest = "devxp/devxp-linux"
+        )
+    ) {
+        triggers {
+            vcs {
+                branchFilter = "+:<default>"
+                quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
+            }
+        }
+    }
+
+    listOf(codeReview, sonarScan, publishDocs).forEach { buildType(it) }
 }
