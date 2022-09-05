2cce3f600449bcf9257e4c58a8a5a0ee6721a45a
Michael Akinde <michael.akinde@statnett.no>
2022-09-05 23:13:54 +0200

    Update TeamCity settings to version 0.41.0

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,10 +3,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
-import no.elhub.common.build.configuration.CodeReview
-import no.elhub.common.build.configuration.ProjectType
-import no.elhub.common.build.configuration.PublishDocs
-import no.elhub.common.build.configuration.SonarScan
+import no.elhub.devxp.build.configuration.CodeReview
+import no.elhub.devxp.build.configuration.ProjectType
+import no.elhub.devxp.build.configuration.PublishDocs
+import no.elhub.devxp.build.configuration.SonarScan
 
 version = "2022.04"
 
