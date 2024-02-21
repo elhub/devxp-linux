133cf48ce3d1e642fefeb8e87a9d5ee4be406afc
Michael Kardzhilov <michael.kardzhilov@statnett.no>
2024-02-21 12:54:18 +0100

    Fix zsh detection

--- scripts/rc-injecter.sh
+++ scripts/rc-injecter.sh
@@ -19,49 +19,65 @@ add_or_edit_line() {
     fi
 }
 
-# Add or edit the line in .bashrc if Bash shell is being used
+# Bash
 if [ -n "$BASH_VERSION" ]; then
     add_or_edit_line "$HOME/.bashrc" "$script_to_trigger"
 fi
 
-# Add or edit the line in .zshrc if Zsh shell is being used
-if [ -n "$ZSH_VERSION" ]; then
+# Zsh
+if [[ "$SHELL" == *"zsh"* ]]; then
+    echo "Zsh detected. Triggering..."
     add_or_edit_line "$HOME/.zshrc" "$script_to_trigger"
+    echo "Line added to .zshrc"
 fi
 
 # Fish
 if command -v fish &> /dev/null; then
+    echo "Fish detected. Triggering..."
     add_or_edit_line "$HOME/.config/fish/config.fish" "$script_to_trigger"
+    echo "Line added to config.fish"
 fi
 
 # Ksh
 if [ -n "$KSH_VERSION" ]; then
+    echo "Ksh detected. Triggering..."
     add_or_edit_line "$HOME/.kshrc" "$script_to_trigger"
+    echo "Line added to .kshrc"
 fi
 
 # Dash
 if [ -n "$DASH_VERSION" ]; then
+    echo "Dash detected. Triggering..."
     add_or_edit_line "$HOME/.dashrc" "$script_to_trigger"
+    echo "Line added to .dashrc"
 fi
 
 # Csh
 if [ -n "$CSH_VERSION" ]; then
+    echo "Csh detected. Triggering..."
     add_or_edit_line "$HOME/.cshrc" "$script_to_trigger"
+    echo "Line added to .cshrc"
 fi
 
 # Tcsh
 if [ -n "$TCSH_VERSION" ]; then
+    echo "Tcsh detected. Triggering..."
     add_or_edit_line "$HOME/.tcshrc" "$script_to_trigger"
+    echo "Line added to .tcshrc"
 fi
 
 # PowerShell
 if [ -n "$PSVersionTable" ]; then
+    echo "PowerShell detected. Triggering..."
     add_or_edit_line "$HOME/.bash_profile" "$script_to_trigger"
+    echo "Line added to .bash_profile"
 fi
 
 # Elvish
 if command -v elvish &> /dev/null; then
+    echo "Elvish detected. Triggering..."
     add_or_edit_line "$HOME/.elvish/rc.elv" "$script_to_trigger"
+    echo "Line added to rc.elv"
 fi
 
 # Add checks for other shell types if needed
